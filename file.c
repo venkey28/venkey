d9c9680943632fb2a9444e36a85f6dc2c408ed28
venkey28 <50804703+venkey28@users.noreply.github.com>
2019-05-19 22:30:19 +0530

    Create file.c

---
+++ file.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main()
+{
+int i;
+if(scanf("%d",&i) != 1){
+printf("error");}
+else{
+if(i==0){
+printf("Zero");}
+else if(i>0){
+printf("Positive");}
+else{
+printf("Negative");}
+}}
